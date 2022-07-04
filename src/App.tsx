6bdfc1bee1b7dca8d008d09469c61dd35cf202de
Mr Coxall <mr.coxall@mths.ca>
2022-07-04 21:03:39 +0000

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react"
+import logo from "./logo.svg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World!
-        </p>
+        <p>Hello, World!</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
